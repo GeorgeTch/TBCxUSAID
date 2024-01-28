3aed48be64d9270f847ed1c9ae5f18f875532c29
George <georgetchintcharauli@gmail.com>
2024-01-28 14:36:06 +0400

    update sidebar effect

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -2,13 +2,17 @@ const closeIcon = document.querySelector('.close-icon');
 const closeBtn = document.querySelector('.close-btn');
 const sidebar = document.querySelector('.terms-and-conditions-sidebar');
 const termsBtn = document.querySelector('.terms-and-conditions');
+const sidebarOuterSpace = document.querySelector('.sidebar-outer-container')
 
 
 function openSidebar() { 
   sidebar.classList.add('displayed-sidebar');
+  sidebarOuterSpace.classList.add('sidebar-outside-bg');
 }
 
 function closeSidebar() {
   sidebar.classList.remove('displayed-sidebar');
+  sidebarOuterSpace.classList.remove('sidebar-outside-bg');
 }
+
 
